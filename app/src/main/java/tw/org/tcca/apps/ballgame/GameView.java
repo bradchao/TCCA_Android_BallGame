4fa6579de5463993fb7e100ca2bd072e05aa791f
bradchao <brad@brad.tw>
2020-08-29 14:13:28 +0800

    2020083003

--- app/src/main/java/tw/org/tcca/apps/ballgame/GameView.java
+++ app/src/main/java/tw/org/tcca/apps/ballgame/GameView.java
@@ -1,14 +1,29 @@
 package tw.org.tcca.apps.ballgame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Bitmap ball;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        setBackgroundColor(Color.BLACK);
+
+        ball = BitmapFactory.decodeResource(context.getResources(), R.drawable.ball);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(ball, 100, 100, null);
     }
 }
